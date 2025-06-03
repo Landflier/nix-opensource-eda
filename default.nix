6b2461d89e37c7b09372665f755d72c83686d51d
Vasil Yordanov <vasilradkovyo@gmail.com>
2025-06-03 22:57:47 +0300

    CHANGE: the nix shell envrionemnt default.nix changed to use callPackage, which is a cleaner solution than the one before

--- default.nix
+++ default.nix
@@ -1,25 +1,62 @@
 { pkgs ? import <nixpkgs> {} }:
 
 let
-  inherit (pkgs) lib;
+  inherit (pkgs) lib callPackage runCommand symlinkJoin;
 
   # Import individual package derivations
-  magic-vlsi = import ./pkgs/magic-vlsi { inherit pkgs; };
-  netgen = import ./pkgs/netgen { inherit pkgs; };
-  yosys = import ./pkgs/yosys { inherit pkgs; };
-  openroad = import ./pkgs/openroad { inherit pkgs; };
-  klayout = import ./pkgs/klayout { inherit pkgs; };
-  open_pdks = import ./pkgs/open_pdks { inherit pkgs; };
-  ngspice = import ./pkgs/ngspice { inherit pkgs; };
+  magic-vlsi = callPackage ./pkgs/magic-vlsi {};
+  irsim = callPackage ./pkgs/irsim {};
+  netgen = callPackage ./pkgs/netgen {};
+  yosys = callPackage ./pkgs/yosys {};
+  openroad = callPackage ./pkgs/openroad {};
+  klayout = callPackage ./pkgs/klayout {};
+  open_pdks = callPackage ./pkgs/open_pdks {};
+  ngspice = callPackage ./pkgs/ngspice {};
+
+  # Function to create a package result directory
+  mkPkgResult = name: pkg: runCommand "${name}-result" {} ''
+    mkdir -p $out/${name}
+    cp -r ${pkg}/* $out/${name}/
+  '';
 
 in {
-  inherit magic-vlsi netgen yosys openroad klayout open_pdks ngspice;
+  inherit magic-vlsi irsim netgen yosys openroad klayout open_pdks ngspice;
 
   # Create a combined package with all tools
   all = pkgs.buildEnv {
     name = "eda-tools";
     paths = [
       magic-vlsi
+      irsim
+      netgen
+      yosys
+      openroad
+      klayout
+      open_pdks
+      ngspice
+    ];
+  };
+
+  # Create structured output with each package in its own directory
+  result = symlinkJoin {
+    name = "eda-tools-result";
+    paths = [
+      (mkPkgResult "magic-vlsi" magic-vlsi)
+      (mkPkgResult "irsim" irsim)
+      (mkPkgResult "netgen" netgen)
+      (mkPkgResult "yosys" yosys)
+      (mkPkgResult "openroad" openroad)
+      (mkPkgResult "klayout" klayout)
+      (mkPkgResult "open_pdks" open_pdks)
+      (mkPkgResult "ngspice" ngspice)
+    ];
+  };
+
+  # Shell environment
+  shell = pkgs.mkShell {
+    buildInputs = [
+      magic-vlsi
+      irsim
       netgen
       yosys
       openroad
